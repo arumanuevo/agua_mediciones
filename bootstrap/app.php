def46ae54e5261734d6bb1539d17418700cffe37
arumanuevo <glipischeck@gmail.com>
2025-10-28 23:13:10 -0300

    app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(append: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
